9f55d97ac5241270b97b9ff9bd49e3a4038df636
Oleg <prostooleg1@gmail.com>
2017-05-09 18:58:38 +0300

    add conftest.py for share fixture

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,14 +1,4 @@
-import pytest
-
-from fixture.Application import Application
 from model.contact import Contact
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
     
 def test_add_contact(app):
