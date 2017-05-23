45c4a3ba8d055ddec3a9920d094169cd4e661d4d
Oleg <prostooleg1@gmail.com>
2017-05-23 19:23:20 +0300

    add comment

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -36,6 +36,7 @@ test_data_five_group = [Contact(firstname="", lastname="", address="")] + [
 #     old_contacts.append(contact)
 #     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
+# берем данные с contacts.json
 def test_add_contact(app, json_contacts):
     contact = json_contacts
     old_contacts = app.contact.get_contact_list()
