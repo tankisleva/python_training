0a2cd4bc89e6ec58e497a22bc4a03829e37a4bab
Oleg <prostooleg1@gmail.com>
2017-05-23 20:10:19 +0300

    edit test_deatails_on_contact_tabe

--- test/test_contact_details.py
+++ test/test_contact_details.py
@@ -1,10 +1,8 @@
 import re
 
-def test_deatails_on_contact_view_page(app):
+def test_deatails_on_contact_tabe(app):
    contact_from_edit_page = app.contact.get_contact_into_from_edit_page(0)
    contact_from_table_list = app.contact.get_contact_list_all_data_from_table()[0]
-   contact_from_table_list.allphones
-   contact_from_table_list.allemails
 
    firstname = contact_from_edit_page.firstname
    lastname = contact_from_edit_page.lastname
