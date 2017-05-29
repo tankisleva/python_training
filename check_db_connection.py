357e8813b6610bddb83176a345b8868583fbde5e
Oleg <prostooleg1@gmail.com>
2017-05-29 19:15:26 +0300

    created orm

--- check_db_connection.py
+++ check_db_connection.py
@@ -2,9 +2,10 @@
 import pymysql.cursors
 
 from fixture.db import DbFixture
+from fixture.orm import ORMFixture
 
 # connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="")
-db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 #connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
 
 # try:
@@ -17,9 +18,9 @@ db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 
 try:
-    contacts = db.get_contact_list()
-    for contact in contacts:
-        print(contact)
-    print(len(contacts))
+    l = db.get_group_list()
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    db.destroy()
+    pass #db.destroy()
